c137d490c1d15621044e8a3ba1eff5140548725c
Alex Ballmer <gnu3ra@riseup.net>
2022-09-05 06:02:44 -0500

    working get_me !!

--- examples/auth.rs
+++ examples/auth.rs
@@ -5,6 +5,7 @@ async fn main() -> Result<()> {
     let token = std::env::var("TOKEN")?;
     let bot = Bot::new(token)?;
     let res = bot.get_me().await?;
-    let res = bot.get_updates(Some(0), Some(1), Some(10), None).await?;
+    println!("{}", res.get_username().as_deref().unwrap_or_default());
+    //let res = bot.get_updates(Some(0), Some(1), Some(10), None).await?;
     Ok(())
 }
