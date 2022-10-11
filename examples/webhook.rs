f90259df9035a8d6731415f668697bece936e534
Alex Ballmer <gnu3ra@riseup.net>
2022-10-11 05:35:40 -0500

    CRYYY forgot to add webhook example I am really sowwy

---
+++ examples/webhook.rs
@@ -0,0 +1,24 @@
+use anyhow::Result;
+use botapi::{
+    bot::Bot,
+    ext::{BotUrl, Webhook},
+};
+use futures_util::stream::StreamExt;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let token = std::env::var("TOKEN")?;
+    let url = std::env::var("URL")?;
+
+    let url = BotUrl::Host(format!("{}", url));
+    let bot = Bot::new(token)?;
+
+    let addr = ([0, 0, 0, 0], 8080).into();
+    let poller = Webhook::new(&bot, url, false, addr);
+    let mut res = poller.get_updates().await?;
+
+    while let Some(Ok(update)) = res.next().await {
+        println!("update {:?}", update);
+    }
+    Ok(())
+}
