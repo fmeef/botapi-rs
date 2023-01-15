ea78b9467c22b1ebbe0a04128f1390223dbfb8e1
fmeef <gnu3ra@riseup.net>
2023-01-15 12:56:42 -0600

    don't run build script if content isn't changed

--- build.rs
+++ build.rs
@@ -5,15 +5,17 @@ use std::process::Command;
 use tggen::Generate;
 
 fn main() -> Result<()> {
+    println!("cargo:rerun-if-changed=generate/");
+    println!("cargo:rerun-if-changed=telegram-bot-api-spec/");
     let json = fs::read_to_string("./telegram-bot-api-spec/api.json")?;
 
     let gen = Generate::new(json)?;
     let types = gen.generate_types()?;
     let methods = gen.generate_methods()?;
 
-    fs::write("./src/gen_methods.rs", methods)?;
-
     fs::write("./src/gen_types.rs", types)?;
+
+    fs::write("./src/gen_methods.rs", methods)?;
 
     match Command::new("rustfmt")
         .args(["--edition", "2021", "./src/gen_methods.rs"])
